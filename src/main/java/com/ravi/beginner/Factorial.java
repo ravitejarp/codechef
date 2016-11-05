7ba8e5ef03d8a8510042aafb7294ece2cafe88f0
ravitejarp <raviteja.rp@gmail.com>
2016-11-05 19:59:11 +0530

    Changes in facto

--- src/main/java/com/ravi/beginner/Factorial.java
+++ src/main/java/com/ravi/beginner/Factorial.java
@@ -1,26 +1,26 @@
-package com.ravi.beginner;
-
-import java.math.BigInteger;
-import java.util.Scanner;
-
-/**
- * Created by ravra03 on 9/15/2016.
- */
-public class Factorial {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int counter = sc.nextInt();
-        while (counter >= 1) {
-            final int facto = sc.nextInt();
-            System.out.println(getTheFactorial(facto));
-            counter--;
-        }
-    }
-
-    private static int getTheFactorial(BigInteger facto) {
-        if (facto.equals(1)) {
-            return 1;
-        }
-        return facto * getTheFactorial(facto.subtract(new BigInteger(1l)));
-    }
-}
+//package com.ravi.beginner;
+//
+//import java.math.BigInteger;
+//import java.util.Scanner;
+//
+///**
+// * Created by ravra03 on 9/15/2016.
+// */
+//public class Factorial {
+//    public static void main(String[] args) {
+//        Scanner sc = new Scanner(System.in);
+//        int counter = sc.nextInt();
+//        while (counter >= 1) {
+//            final int facto = sc.nextInt();
+//            System.out.println(getTheFactorial(facto));
+//            counter--;
+//        }
+//    }
+//
+//    private static int getTheFactorial(BigInteger facto) {
+//        if (facto.equals(1)) {
+//            return 1;
+//        }
+//        return facto * getTheFactorial(facto.subtract(new BigInteger(1l)));
+//    }
+//}
