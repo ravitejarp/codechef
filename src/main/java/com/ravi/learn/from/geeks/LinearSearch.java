4c386a1e97bea21a447162d1806901082ef334a8
ravitejarp <raviteja.rp@gmail.com>
2016-11-05 20:09:45 +0530

    LinearSearch

---
+++ src/main/java/com/ravi/learn/from/geeks/LinearSearch.java
@@ -0,0 +1,42 @@
+package com.ravi.learn.from.geeks;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by ravra03 on 11/5/2016.
+ */
+public class LinearSearch {
+    public static void main(String[] args) {
+        int[] arr = {21, 2, 121, 21, 314324, 3};
+        List<Integer> arrayList = new ArrayList<Integer>();
+        arrayList.add(12);
+        arrayList.add(112);
+        arrayList.add(122);
+        arrayList.add(1112);
+        arrayList.add(412);
+        LinearSearch ls = new LinearSearch();
+        System.out.println("The index of 21 is:" + ls.linearSearch(arr, 21));
+        System.out.println("The index of 412 is:" + ls.linearSearch(arrayList, 412));
+
+    }
+
+    public int linearSearch(int[] arr, int key) {
+        for (int num = 0; num < arr.length; num++) {
+            if (arr[num] == key) {
+                return num;
+            }
+        }
+        return -1;
+    }
+
+    public int linearSearch(List<Integer> arr, int key) {
+        for (int num = 0; num < arr.size(); num++) {
+            if (arr.get(num) == key) {
+                return num;
+            }
+        }
+        return -1;
+    }
+
+}
